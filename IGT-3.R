c989662bbabcd8aa3071d116b5c04ca24ecc65dc
FilipGaida99 <fgaida99@outlook.com>
2022-05-22 04:56:08 -0400

    forward backward

--- IGT-3.R
+++ IGT-3.R
@@ -1,9 +1,12 @@
+# a)
 set.seed(1)
 X <- rnorm(100)
 noise <- rnorm(100)
 Beta <- c(0.2137,0.420,0.911,0.69)
+# b)
 Y <- Beta[1] + Beta[2] *X + Beta[3] *X^2 - Beta[4] *X^3 + noise
 
+# c)
 require(leaps)
 df <- data.frame(Y, X)
 fit <- regsubsets(Y ~ poly(X, 10), data = df, nvmax = 10)
@@ -21,4 +24,32 @@ data_frame(Cp = fit_summary$cp,
   geom_line() + geom_point() + ylab('') + xlab('Number of Variables Used') +
   facet_wrap(~ value_type, scales = 'free') +
   theme_tufte() + scale_x_continuous(breaks = 1:10)
+
+# d)
+#backward
+require(caret)
+
+model_back <- train(Y ~ poly(X, 10), data = df, 
+                    method = 'glmStepAIC', direction = 'backward', 
+                    trace = 0,
+                    trControl = trainControl(method = 'none', verboseIter = FALSE))
+
+postResample(predict(model_back, df), df$Y)
+
+summary(model_back$finalModel)
+
+#forward
+
+x_poly <- poly(df$X, 10)
+
+colnames(x_poly) <- paste0('poly', 1:10)
+model_forw <- train(y = Y, x = x_poly,
+                    method = 'glmStepAIC', direction = 'forward',
+                    trace = 0,
+                    trControl = trainControl(method = 'none', verboseIter = FALSE))
+
+postResample(predict(model_forw, data.frame(x_poly)), df$Y)
+
+summary(model_forw$finalModel)
+
 
