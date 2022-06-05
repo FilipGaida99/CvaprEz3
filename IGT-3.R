22b61e479a29a8c830d206d402bd2a0107a6af66
FilipGaida99 <fgaida99@outlook.com>
2022-06-05 11:00:35 -0400

    last task

--- IGT-3.R
+++ IGT-3.R
@@ -100,32 +100,44 @@ predict(lasso, s = best_lambda, type = "coefficients")[1:11, ]
 # f)
 Beta[7] <- 5
 Y_7 <- Beta[1] + Beta[7]*X^7 + noise
-df_2 <- data_frame(Y_7 = Y_7, X = df[,-1])
+df_2 <- data_frame(Y_7 = Y_7, X = X)
 
 fit <- regsubsets(Y_7 ~ poly(X, 10), data = df_2, nvmax = 10)
 
 fit_summary <- summary(fit)
 
-data_frame(Cp = fit_summary$cp,
-           BIC = fit_summary$bic,
-           R2 = fit_summary$adjr2) %>%
-  mutate(id = row_number()) %>%
-  gather(value_type, value, -id) %>%
-  ggplot(aes(id, value, col = value_type)) +
-  geom_line() + geom_point() + ylab('') + xlab('Number of Variables Used') +
-  facet_wrap(~ value_type, scales = 'free') +
-  theme_tufte() + scale_x_continuous(breaks = 1:10)
+par(mfrow = c(1, 3))
 
-lasso_y7_model <- train(Y_7 ~ poly(X, 10), data = df_2,
-                        method = 'glmnet', 
-                        trControl = trainControl(method = 'cv', number = 5),
-                        tuneGrid = expand.grid(alpha = 1, 
-                                               lambda = seq(0.001, 0.2, by = 0.005)))
+cp <- fit_summary$cp
+plot(cp, xlab="Variables number", ylab = "cp", type = "l")
+points(which.min(cp), cp[which.min(cp)], col = "red", cex = 1.5, pch = 1)
 
-plot(lasso_y7_model)
+bic <- fit_summary$bic
+plot(bic, xlab="Variables number", ylab = "bic", type = "l")
+points(which.min(bic), bic[which.min(bic)], col = "red", cex = 1.5, pch = 1)
 
-plot(varImp(lasso_y7_model))
+adjr2 <- fit_summary$adjr2
+plot(adjr2, xlab="Variables number", ylab = "adjr2", type = "l")
+points(which.max(adjr2), adjr2[which.max(adjr2)], col = "red", cex = 1.5, pch = 1)
 
-coef(lasso_y7_model$finalModel, lasso_y7_model$bestTune$lambda)
+coef(fit, which.min(cp))
+coef(fit, which.min(bic))
+coef(fit, which.max(adjr2))
 
-postResample(predict(lasso_y7_model, df_2), df_2$Y_7)
+fit_summary <- summary(fit)
+
+
+#lasso 7
+library(glmnet)
+set.seed(1)
+par(mfrow = c(1,1))
+
+xmat <- model.matrix(Y_7 ~ poly(X, 10), data = df_2)[, -1]
+crossval_lasso <- cv.glmnet(xmat, Y_7, alpha = 1)
+
+plot(crossval_lasso)
+
+best_lambda <- crossval_lasso$lambda.min
+
+lasso <- glmnet(xmat, Y_7, alpha = 1)
+predict(lasso, s = best_lambda, type = "coefficients")[1:11, ]
